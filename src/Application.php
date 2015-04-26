a6fabdb205f585294bccaf5c0aacbccfc9e4a24c
Dawid Nowak <code@dnowak.pl>
2015-04-26 23:24:37 +0200

    init cilex app

---
+++ src/Application.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MacDada\Wykop\AspieStats;
+
+class Application extends \Cilex\Application
+{
+    public function __construct()
+    {
+        parent::__construct('WykopAspieStats');
+    }
+}
